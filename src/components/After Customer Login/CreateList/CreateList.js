f0980362351139405a0ef73481c879c38a023c50
Akash Kumar <mailakash3103@gmail.com>
2022-04-28 19:33:29 +0530

    bydefault low

--- src/components/After Customer Login/CreateList/CreateList.js
+++ src/components/After Customer Login/CreateList/CreateList.js
@@ -80,6 +80,16 @@ const useStyles = makeStyles({
     fontSize: "16px",
   },
   rowfield: {
+    "&:hover": {
+      borderColor: "#cdcdcd !important",
+    },
+    "& .MuiFilledInput-root": {
+      backgroundColor: "white",
+      "&::after ,::before": { display: "none" },
+      "& .MuiFilledInput-input": {
+        "&:focus": { backgroundColor: "transparent" },
+      },
+    },
     "& label.Mui-focused": {
       color: "red",
       background: "#fff",
@@ -100,12 +110,10 @@ const useStyles = makeStyles({
       "&.Mui-focused fieldset": {
         borderColor: "#cdcdcd",
       },
-      "& .MuiFilledInput-root": {
-        backgroundColor: "white",
-        "&::after ,::before": { display: "none" },
-        "& .MuiFilledInput-input": {
-          "&:focus": { backgroundColor: "transparent" },
-        },
+
+      "&::after ,::before": { display: "none" },
+      "& .MuiFilledInput-input": {
+        "&:focus": { backgroundColor: "transparent" },
       },
     },
     icons: {
@@ -117,7 +125,7 @@ const CreateList = ({ FirstPageAction }) => {
   const classes = useStyles();
   const [high, setHigh] = useState(false);
   const [medium, setMedium] = useState(false);
-  const [low, setLow] = useState(false);
+  const [low, setLow] = useState(true);
   const [files, setFiles] = useState([]);
   const [open, setOpen] = useState(false);
   const [loader, setLoader] = useState(false);
@@ -521,6 +529,9 @@ const CreateList = ({ FirstPageAction }) => {
                 // variant="outlined"
               /> */}
                 <TextField
+                  sx={{
+                    "&:hover": { borderColor: "none" },
+                  }}
                   label="Details"
                   variant="outlined"
                   multiline
