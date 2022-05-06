9e762bf8cb08a45f0e4cb478acb8bbab26d2c355
Akash Kumar <mailakash3103@gmail.com>
2022-05-06 16:24:20 +0530

    Admin Manage Customer Account Request design fix

--- src/common/img-icon.jsx
+++ src/common/img-icon.jsx
@@ -1,5 +1,5 @@
 const ImgIcon = ({ children, alt = "" }) => (
-  <img src={children} alt={alt} width="20" height="20" />
+  <img src={children} alt={alt} width="15px" height="15px" />
 );
 
 export default ImgIcon;
